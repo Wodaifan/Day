d63792bb1a936a3b36df76f6c8888c885d74afdd
wodaifan <wodaifan@163.com>
2025-07-05 21:51:51 +0800

    commit Day2

--- Day2/rec2.cpp
+++ Day2/rec2.cpp
@@ -1,3 +1,25 @@
 //
-// Created by Ê≠¶‰æç on 2025/7/5.
+// Created by Œ‰ Ã on 2025/7/5.
 //
+#include "iostream"
+
+using namespace std;
+
+int f(int n, int k);
+
+int main() {
+    cout << "«Î ‰»În”Îk" << endl;
+    int n, k;
+    cin >> n;
+    cin >> k;
+    cout << f(n, k) << endl;
+    return 0;
+}
+
+int f(int n, int k) {
+    if ((n == k) || (k == 0)) {
+        return 1;
+    } else {
+        return f(n - 1, k - 1) + f(n - 1, k);
+    }
+}
